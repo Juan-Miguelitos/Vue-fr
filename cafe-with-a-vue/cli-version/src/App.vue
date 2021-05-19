57322b558c207d72fb5678bde3269fa061443537
Juan-Miguelitos <73351499+Juan-Miguelitos@users.noreply.github.com>
2021-05-19 17:11:59 +0200

    Update App.vue

--- cafe-with-a-vue/cli-version/src/App.vue
+++ cafe-with-a-vue/cli-version/src/App.vue
@@ -7,7 +7,30 @@
 				matin ou avec un goûter réconfortant. Mais attention, vous verrez qu'il
 				est difficile de s'arrêter.
 			</p>
-  </div>
+
+      <section class="menu">
+				<h2>Menu</h2>
+
+			</section>
+
+      <aside class="shopping-cart">
+				<h2>Panier d'achat : {{ shoppingCart }} articles</h2>
+			</aside>
+
+			<h2>Contactez nous</h2>
+			<p>Adresse : {{ address }}</p>
+			<p>Téléphone : {{ phone }}</p>
+			<p>Email : {{ email }}</p>
+			<p>Horaires :</p>
+			<ul>
+				<li>L-V: 06:00 à 16:00</li>
+				<li>Samedi: 07:00 à 14:00</li>
+				<li>Dimanche: 07:00 à 12:00</li>
+			</ul>
+			<footer class="footer">
+				<p>{{ copyright }}</p>
+			</footer>
+		</div>
 </template>
 
 <script>
