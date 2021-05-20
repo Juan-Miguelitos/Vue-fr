826ffb4e2552511c900b33603c4340efc875e0b0
Juan-Miguelitos <73351499+Juan-Miguelitos@users.noreply.github.com>
2021-05-20 09:24:06 +0200

    Update App.vue

--- cafe-with-a-vue/cli-version/src/App.vue
+++ cafe-with-a-vue/cli-version/src/App.vue
@@ -78,6 +78,7 @@ export default {
 						}
 					]
 				},
+  },
 				computed: {
 					copyright() {
 						const currentYear = new Date().getFullYear()
@@ -90,7 +91,6 @@ export default {
 						this.shoppingCart += amount
 					}
 				}
-	}
 }
 
 
